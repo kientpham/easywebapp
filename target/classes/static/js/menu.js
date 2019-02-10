e9f2652a8dcdda9d58103227b1ae1035988ab09a
Kien Pham <kien.t.pham2@boeing.com>
2019-02-09 16:03:37 -0800

    Apply cooladmin
    Apply cooladmin

--- target/classes/static/js/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function(){
-    $(".dropdown").hover(
-        function() { $('.dropdown-menu', this).fadeIn("fast");
-        },
-        function() { $('.dropdown-menu', this).fadeOut("fast");
-    });
-});
